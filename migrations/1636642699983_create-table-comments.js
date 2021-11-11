3d3de568ae63b7a7983402b78ff25555eecffbef
FathirRD <fathirrahmand@gmail.com>
2021-11-12 02:18:11 +0700

    create migrations

---
+++ migrations/1636642699983_create-table-comments.js
@@ -0,0 +1,46 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    thread: {
+      type: 'VARCHAR(50)',
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+    },
+    is_deleted: {
+      type: 'boolean',
+    },
+  });
+
+  pgm.addConstraint(
+    'comments', 'fk_comments.thread_threads.id', 'FOREIGN KEY(thread) REFERENCES threads(id) ON DELETE CASCADE',
+    'comments', 'fk_comments.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE',
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('comments', 'fk_comments.thread_threads.id');
+  pgm.dropConstraint('comments', 'fk_comments.owner_users.id');
+  pgm.dropTable('threads');
+};
